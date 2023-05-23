22ac064f2577fb5eb9814bdbb61c1eb5809a89cb
Danil Vorontsov <morphengine69@yandex.ru>
2023-05-23 15:55:30 +0700

    Added a feature categories
    Took 1 minute

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from reviews.models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    """Вьюсет для обработки категорий для произведений."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+    filter_backends = [filters.SearchFilter, ]
+    search_fields = ['name', ]
+    lookup_field = 'slug'
+    http_method_names = ['get', 'post', 'delete', ]
