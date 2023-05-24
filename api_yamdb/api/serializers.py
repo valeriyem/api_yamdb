a7dc2a6428ae04f30be23a1abf1dee954c6ef688
Danil Vorontsov <morphengine69@yandex.ru>
2023-05-24 17:03:43 +0700

    fix pep8
    Took 11 minutes

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,6 +3,28 @@ from rest_framework import serializers
 from reviews.models import Title, Category, Genre
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    """Сериализация модели категорий к произведениям."""
+    class Meta:
+        model = Category
+        fields = [
+            'id',
+            'name',
+            'slug',
+        ]
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализация модели жанров к произведениям."""
+    class Meta:
+        model = Genre
+        fields = [
+            'id',
+            'name',
+            'slug',
+        ]
+
+
 class TitleSerializer(serializers.ModelSerializer):
     """Сериализация модели произведений."""
     category = CategorySerializer(read_only=True)
@@ -17,28 +39,4 @@ class TitleSerializer(serializers.ModelSerializer):
             'description',
             'category',
             'genre',
-        ]
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    """Сериализация модели категорий к произведениям."""
-
-    class Meta:
-        model = Category
-        fields = [
-            'id',
-            'name',
-            'slug',
-        ]
-
-
-class GenreSerializer(serializers.ModelSerializer):
-    """Сериализация модели жанров к произведениям."""
-
-    class Meta:
-        model = Genre
-        fields = [
-            'id',
-            'name',
-            'slug',
         ]
